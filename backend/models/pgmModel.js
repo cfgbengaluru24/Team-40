db77b3cfcbeabad02b22b2e71f3ef081f3dd2324
kkarthikraj03 <rajkarthik090903@gmail.com>
2024-07-27 14:42:49 +0530

    Pgmcommit

---
+++ backend/models/pgmModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+
+const programSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        trainerId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Trainer', 
+            required: true,
+            default: "defaultTrainerId" 
+        },
+        userIds: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            }
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+
+const Program = mongoose.model('Program', programSchema);
+
+export default Program;
---
+++ backend/routes/pgmRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import Program from '../models/programModel.js'; 
+
+const router = express.Router();
+
+router.post('/add', async (req, res) => {
+    const { name, trainerId, userIds } = req.body;
+
+    try {
+        const program = new Program({
+            name,
+            trainerId,
+            userIds,
+        });
+
+        const createdProgram = await program.save();
+        res.status(201).json(createdProgram);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+export default router;
