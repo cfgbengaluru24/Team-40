d4f403884220408bf8c87e5dbb338031b305a957
kavyas127 <officialkesai315@gmail.com>
2024-07-27 22:45:36 +0530

    frontend commit 4

--- frontend/src/components/LoginSignUp.jsx
+++ frontend/src/components/LoginSignUp.jsx
@@ -7,7 +7,7 @@ var userIsRegistered = true;
 function LoginSignUp() {
     return (
         <div className="container">
-            <h1 className="user-title2">{userIsRegistered? "Welcome Back!" : "Hello!"}</h1>
+            <h1 className="user-title2">{userIsRegistered? "Welcome Back!" : "Welcome!"}</h1>
             <Form text={userIsRegistered} />
         </div>
     );
