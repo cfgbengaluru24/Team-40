8d5414571d08d18c07f8e48271f3ebb1e0382b74
kavyas127 <officialkesai315@gmail.com>
2024-07-27 22:11:53 +0530

    frontend commit 3

--- frontend/src/components/Trainer/Dashboard.jsx
+++ frontend/src/components/Trainer/Dashboard.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
 import NavbarTrainer from '../NavbarTrainer'
+import ListOfUsers from './ListOfUsers'
+import ListOfInchargeProgram from './ListOfInchargeProgram'
 
 const Dashboard = () => {
   return (
     <div>
       <Header />
       <NavbarTrainer />
+      <ListOfInchargeProgram />
+      {/* <ListOfUsers /> */}
       <Footer />
     </div>
   )
